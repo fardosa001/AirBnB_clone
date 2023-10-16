95f155dfe500e4713449ca1d000c867205c747e4
fardosa001 <fardosaadow@gmail.com>
2023-10-16 11:42:17 -0700

    add tests for fileStorage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+import unittest
+import os
+from models.engine.file_storage import FileStorage
+from models.user import User
+import json
+
+class TestFileStorage(unittest.TestCase):
+    """class tests"""
+    def setUp(self):
+        """setup class"""
+        self.storage = FileStorage()
+        self.user = User()
+        self.path = "file.json"
+
+    def tearDown(self):
+        """teardown test at the end"""
+        del self.user
+        if os.path.exists("file.json"):
+            os.remove("file.json")
+
+    def test_all(self):
+        """tests for all method"""
+        objects = self.storage.all()
+        self.assertIsInstance(objects, dict)
+        self.assertEqual(len(objects), 1)
+
+    def test_new(self):
+        """test for new created"""
+        self.storage.new(self.user)
+        objects = self.storage.all()
+        self.assertEqual(len(objects), 2)
+
+    def test_save(self):
+        """test for save method"""
+        self.storage.new(self.user)
+        self.storage.save()
+        self.assertTrue(os.path.exists(self.path))
+        
+    def test_reload(self):
+        """test for reload method"""
+        self.storage.new(self.user)
+        self.storage.save()
+        self.storage.reload()
+        objects = self.storage.all()
+        self.assertEqual(len(objects), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
