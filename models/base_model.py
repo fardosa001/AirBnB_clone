f2ecbde8bf7f6da3f2e2469ecf0744de904d5283
fardosa001 <fardosaadow@gmail.com>
2023-10-16 03:32:25 -0700

    update BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -25,11 +25,11 @@ class BaseModel:
                     setattr(self, key, datetime.isoformat(val))
                 elif key != '__class__':
                     setattr(self, key, val)
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-        models.storage.new(self)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """string representation of the class"""
