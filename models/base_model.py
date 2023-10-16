40fbe6d6e7f0426b88ff14cb2d71245fb73c9bdd
fardosa001 <fardosaadow@gmail.com>
2023-10-16 03:28:43 -0700

    update BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -22,19 +22,18 @@ class BaseModel:
         if kwargs:
             for key, val in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime
-                            (val, '%Y-%m-%dT%H:%M:%S.%f'))
+                    setattr(self, key, datetime.isoformat(val))
                 elif key != '__class__':
                     setattr(self, key, val)
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.updated_at = self.created_at
         models.storage.new(self)
 
     def __str__(self):
         """string representation of the class"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+        return "[{}] ({}) {}".format(type(self).__name__, self.id,
                                      self.__dict__)
 
     def save(self):
